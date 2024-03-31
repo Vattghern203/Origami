3f52762c70071ddb89c2d3b6f4cd6e2ceacf40b8
Otávio Augusto <otavioaugustobrb19@gmail.com>
2024-03-31 10:30:23 -0300

    feat: scale factors

--- src/pdf.py
+++ src/pdf.py
@@ -1,54 +1,32 @@
 import PyPDF2
-
-from typing import Literal
-
 import os
 
-from utils.bcolors import bcolors
-
 from progress.bar import Bar
-
-""" def resize_pdf_to_a4(input_path, output_path):
-    with open(input_path, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-        writer = PyPDF2.PdfWriter()
-
-        for page_number in range(len(reader.pages)):
-            page = reader.pages[page_number]
-            page.mediabox.lower_left = (0, 0)
-            page.mediabox.upper_right = (595, 842)  # A4 size: 210 x 297 mm
-
-            writer.add_page(page)
-
-        with open(output_path, 'wb') as output_file:
-            writer.write(output_file)
-
-# Example usage
-input_file_path = 'essay.pdf'
-output_file_path = f'converted/resized.pdf'
-
-resize_pdf_to_a4(input_file_path, output_file_path)
-print("PDF resized successfully.") """
+from typing import Literal
+from utils.bcolors import bcolors
 
 class ResizePDF:
 
-    def __init__(self, input_path: str, output_path: str, desired_format: Literal["A4", "A3", "L13"]) -> None:
-        
+    def __init__(self, input_path: str, output_path: str, desired_format: Literal["A2","A3", "A4", "A5", "L13"]) -> None:
+
         self.__input_path = input_path
         self.__input_filename = os.path.basename(input_path)
         self.__output_path = f'{output_path}{self.__input_filename}'
-
-        print(self.__output_path)
-
         self.__desired_format = desired_format
         self.__desired_doc_type = 'pdf'
 
+        self.__MEASEURE_POINTS = 2.83464567
+
         self.__formats = {
-            "A4": (210, 297)
+            "A2": (420, 594),
+            "A3": (297, 420),
+            "A4": (210, 297),
+            "A5": (148, 210)
         }
 
-        os.makedirs(output_path, exist_ok=True)
+        self.__desired_dimensions = self.mm_to_point_transformation()
 
+        os.makedirs(output_path, exist_ok=True)
 
     def is_pdf(self, file: str) -> bool:
         """Check if a file is a PDF.
@@ -63,7 +41,6 @@ class ResizePDF:
         filename = file or self.__input_filename
 
         return filename.split('.')[-1].lower() == self.__desired_doc_type
-    
 
     def retrieve_pdfs_per_folder(self) -> list:
         """Retrieve a list of PDF files in the input folder.
@@ -78,8 +55,8 @@ class ResizePDF:
 
         for doc in document_list:
 
-            if self.is_pdf(doc): 
-                
+            if self.is_pdf(doc):
+
                 pdf_list.append(
                     {
                         "name": doc,
@@ -91,12 +68,39 @@ class ResizePDF:
 
         return pdf_list
 
+    # def __handle_scale_factor(self, width: float, height: float):
 
-    def  __handle_resize(
-            self, 
-            pdf_file_path: str, 
-            output_path: str
-        ):
+    def mm_to_point_transformation(self) -> tuple[float, float]:
+
+        final_width = round(
+            self.__formats[self.__desired_format][0] * self.__MEASEURE_POINTS)
+
+        final_height = round(
+            self.__formats[self.__desired_format][1] * self.__MEASEURE_POINTS)
+
+        return (final_width, final_height)
+    
+
+    def get_scale_factor(self, original_dimension: tuple[float, float]) -> tuple[float, float]:
+
+        scale_factor_w = self.__desired_dimensions[0] / original_dimension[0]
+        scale_factor_y = self.__desired_dimensions[1] / original_dimension[1]
+
+        scale_factor = (
+            scale_factor_w, 
+            scale_factor_y
+        )
+
+        print(scale_factor)
+
+        return scale_factor
+
+
+    def __handle_resize(
+        self,
+        pdf_file_path: str,
+        output_path: str
+    ):
         """Resize a PDF file to the desired format.
 
         Args:
@@ -108,14 +112,30 @@ class ResizePDF:
             print(f'{bcolors.OKBLUE} Processing [{self.__input_filename}]...')
 
             with open(pdf_file_path, 'rb') as file:
+
                 reader = PyPDF2.PdfReader(file)
                 writer = PyPDF2.PdfWriter()
 
                 for page_number in range(len(reader.pages)):
 
                     page = reader.pages[page_number]
+
+                    original_height = float(page.mediabox.height)
+                    original_width = float(page.mediabox.width)
+
+                    original_dimension = (original_width, original_height)
+
+                    print('Original dimensions',
+                          original_width, original_height)
+
+                    # page.cropbox.scale(5, 5)
+
+                    scale_factor = self.get_scale_factor(original_dimension)
+
+                    page.scale(scale_factor[0], scale_factor[1])
+
                     page.mediabox.lower_left = (0, 0)
-                    page.mediabox.upper_right = (595, 842)  # A4 size: 210 x 297 mm
+                    page.mediabox.upper_right = self.__desired_dimensions  # A4 size: 210 x 297 mm
 
                     writer.add_page(page)
 
@@ -139,18 +159,18 @@ class ResizePDF:
 
         print(f'{bcolors.OKBLUE}Converting {len(pdf_list)} files...')
 
-        progress_bar = Bar(f'{bcolors.OKBLUE}Processing {self.__input_filename}...', max=len(pdf_list))
+        progress_bar = Bar(
+            f'{bcolors.OKBLUE}Processing {self.__input_filename}...', max=len(pdf_list))
 
         for doc in pdf_list:
 
             progress_bar.next()
-            
+
             self.__input_filename = doc["name"]
 
             self.__handle_resize(
                 f'{self.__input_path}{doc["name"]}', doc["path"]
             )
-
 
         progress_bar.finish()
 
@@ -166,8 +186,8 @@ class ResizePDF:
 
         else:
 
-            print(f'{bcolors.FAIL}The file ({self.__input_filename}) is not a PDF or is not supported.')
-
+            print(
+                f'{bcolors.FAIL}The file ({self.__input_filename}) is not a PDF or is not supported.')
 
     def resize(self) -> None:
         """Resize PDF files based on the input path.
@@ -175,7 +195,7 @@ class ResizePDF:
         If the input path is a file, resize the single file.
         If the input path is a directory, resize all PDF files in the directory.
         """
-    
+
         if os.path.isfile(self.__input_path):
 
             self.resize_a_single_file()  # Resize the single file
@@ -186,7 +206,8 @@ class ResizePDF:
 
             if len(pdf_list) == 1:
 
-                self.resize_a_single_file(pdf_list[0])  # Resize the single PDF file
+                # Resize the single PDF file
+                self.resize_a_single_file(pdf_list[0])
             elif len(pdf_list) > 1:
 
                 self.resize_pipeline(pdf_list)  # Resize multiple PDF files
